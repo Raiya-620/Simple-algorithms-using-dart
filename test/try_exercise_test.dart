3f41ece4cba055d587a1a1d952fa44f33c92c7dc
AsumahBanda <117347715+AsumahBanda@users.noreply.github.com>
2024-09-12 15:03:03 +0000

    change test group name

--- test/try_exercise_test.dart
+++ test/try_exercise_test.dart
@@ -1,25 +1,25 @@
 import 'package:try_exercise/try_exercise.dart';
 
 void main() {
-  group('findHighest', () {
+  group('test return maximum number', () {
     test('should return null for an empty list', () {
-      expect(findHighest([]), isNull);
+      expect(returnMaximumNunber([]), isNull);
     });
 
     test('should return the highest number for a list with one element', () {
-      expect(findHighest([5]), 5);
+      expect(returnMaximumNunber([5]), 5);
     });
 
     test('should return the highest number for a list with multiple elements', () {
-      expect(findHighest([1, 5, 3, 9, 2]), 9);
+      expect(returnMaximumNunber([1, 5, 3, 9, 2]), 9);
     });
 
     test('should return the highest number when the highest is the first element', () {
-      expect(findHighest([10, 5, 3, 9, 2]), 10);
+      expect(returnMaximumNunber([10, 5, 3, 9, 2]), 10);
     });
 
     test('should return the highest number when the highest is the last element', () {
-      expect(findHighest([1, 5, 3, 9, 12]), 12);
+      expect(returnMaximumNunber([1, 5, 3, 9, 12]), 12);
     });
   });
 }
